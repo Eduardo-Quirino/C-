1b7fe5366d929b2f33fea65b9f136308fd7d9bd7
Eduardo-Quirino <edu.3532@gmail.com>
2021-06-19 10:58:04 -0300

    Exercício feito 8 e 9

---
+++ assets/1-atividades_assíncronas/exercício 9/exercício 9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Programação n°2
+             Crie um algoritimo que leia o raio e a altura de um cilindro e que calcule e escreva o seu volume e sua área
+             volume = π * raio² * altura
+             area = 2 * π * (altura + raio) 
+             OBS: considere  π = 3.14
+             */
+
+            //ENTRADA
+            double raio, altura, volume, area;
+            Console.WriteLine("Calculo de Raio e area de um CILINDRO");
+            Console.WriteLine("Digite o Raio: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+
+            //PROCESSAMENTO
+
+            volume = Math.PI * Math.Pow(raio,2) * altura;// Math.PI--> calcula direto o valor de PI pelo c# --> Math.Pow==> Retorna um número especificado elevado à potência especificada.
+            Console.WriteLine("O volume do cilindro é: " + Math.Round(volume, 2));
+
+            area = 2 * Math.PI*(altura+raio);// Math.PI--> calcula direto o valor de PI pelo c#
+            Console.WriteLine("A área do cilindro é: " + Math.Round(area,2));
+
+
+            //SAIDA
+            Console.ReadKey();
+        }
+    }
+}
