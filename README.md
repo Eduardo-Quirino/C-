761a307a604b429a55968a3ffcd453789380a1f2
Eduardo Quirino <edu.3532@gmail.com>
2022-02-17 22:05:14 -0300

    Atualização

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# C#
+# C#                                <img src="https://arquivo.devmedia.com.br/cursos/imagem/curso_o-que-e-csharp_1983.png" alt="Guia de Programação em C#: Aprenda C# do Básico ao Avançado" style="zoom: 33%;" />
 aulas de C# ministrado pela instituição Senac em 2021
 
 
 
-## *Matriz*es
+- **Lógica **
 
-<img src="https://arquivo.devmedia.com.br/cursos/imagem/curso_o-que-e-csharp_1983.png" alt="Guia de Programação em C#: Aprenda C# do Básico ao Avançado" style="zoom: 33%;" />
+- **Matrizes**
+
+
 
 Revisão de aulas :
 
---
+++ aplicativos/exercício 4-LerTemeratura/exercício 4-LerTemeratura/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício_4_LerTemeratura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Muitos aises estão assando a utilizar o sistema métrico. Crie um algoritimo que leia uma temeratura dada em graus Farenheit e imprimir o equivalente em Celcius.
+               tc = 5 * (tf-32)/9
+             */
+
+            //entrada
+            double tf, tc;
+
+            Console.WriteLine("Programa de leitura e conversão de temeperatura Farenheit para Celcius");
+           
+            Console.WriteLine("Informe a temperatura em Farenheit:  ");//imprime no console 
+            tf = Convert.ToDouble(Console.ReadLine());//converte valor digitado
+
+            //processamento
+            tc = 5 * (tf-32) /9;//calculo
+
+            //saida
+            Console.WriteLine(tf + "°F equivale a: " + Math.Round (tc,1) + "°C");//imprime resultado 
+            Console.ReadKey();//aguarda
+        }
+    }
+}
